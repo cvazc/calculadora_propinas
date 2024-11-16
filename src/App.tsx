2d097785aaa5b23f6a276eed7d77a716fbd74bd9
secontreras <se.contreras@mavi.mx>
2024-11-15 19:30:55 -0600

    Implement main layout and menu data for tip calculator app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { menuItems } from "./data/db";
+
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <header className="bg-teal-400 py-5">
+                <h1 className="text-center text-4xl font-black">
+                    Calculadora de Propinas y Consumos
+                </h1>
+            </header>
+
+            <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+                <div>
+                    <h2>Menu</h2>
+                </div>
+
+                
+
+                <div>
+                    <h2>Consumo</h2>
+                </div>
+            </main>
         </>
     );
 }
