a7f972b1f4e34618827cf4b82a6e38cef2582a7f
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2024-11-18 18:09:56 -0600

    Add menu item component and order management functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
 
 function App() {
+    const { addItem } = useOrder();
+
     return (
         <>
             <header className="bg-teal-400 py-5">
@@ -10,11 +14,18 @@ function App() {
             </header>
 
             <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-                <div>
-                    <h2>Menu</h2>
+                <div className="p-5">
+                    <h2 className="text-4xl font-black">Menú</h2>
+                    <div className="space-y-3 mt-10">
+                        {menuItems.map((item) => (
+                            <MenuItem
+                                key={item.id}
+                                item={item}
+                                addItem={addItem}
+                            />
+                        ))}
+                    </div>
                 </div>
-
-                
 
                 <div>
                     <h2>Consumo</h2>
