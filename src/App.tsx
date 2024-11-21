196de4ba033cb60e8acff6a6280c5c0a17ffb9b8
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2024-11-20 21:36:44 -0600

    Add tip management and order totals functionality to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-    const { order, addItem, removeItem } = useOrder();
+    const { tip, setTip, order, addItem, removeItem, placeOrder } = useOrder();
 
     return (
         <>
@@ -29,10 +31,24 @@ function App() {
                 </div>
 
                 <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-                        <OrderContents
-                            order={order}
-                            removeItem={removeItem}
-                        />
+                    {order.length ? (
+                        <>
+                            <OrderContents
+                                order={order}
+                                removeItem={removeItem}
+                            />
+
+                            <TipPercentageForm setTip={setTip} tip={tip} />
+
+                            <OrderTotals
+                                order={order}
+                                tip={tip}
+                                placeOrder={placeOrder}
+                            />
+                        </>
+                    ) : (
+                        <p className="text-center">La order esta vacia</p>
+                    )}
                 </div>
             </main>
         </>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,6 +3,7 @@ import type { OrderItem, MenuItem } from "../types";
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]);
+    const [tip, setTip] = useState(0);
 
     const addItem = (item: MenuItem) => {
         const itemExist = order.find((orderItem) => orderItem.id === item.id);
@@ -19,13 +20,21 @@ export default function useOrder() {
         }
     };
 
-    const removeItem = (id : MenuItem['id']) => {
-        setOrder(order.filter(item => item.id !== id))
+    const removeItem = (id: MenuItem["id"]) => {
+        setOrder(order.filter((item) => item.id !== id));
     };
+
+    const placeOrder = () => {
+        setOrder([])
+        setTip(0)
+    }
 
     return {
         order,
+        tip,
+        setTip,
         addItem,
         removeItem,
+        placeOrder
     };
 }
